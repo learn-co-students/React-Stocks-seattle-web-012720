2e45dc6cb895d4f20aaed817f8c9ec4c63f2db02
dianakw8591 <dianakw@gmail.com>
2020-04-06 15:55:12 -0700

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,32 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+
+  const handleChangeSort = event => {
+    props.onChooseSort(event.target.value);
+  }
+
+  const handleChangeFilter = event => {
+    props.onChooseFilter(event.target.value);
+  }
+
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={props.sortBy === "Alphabetically"} onChange={handleChangeSort}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={props.sortBy === "Price"} onChange={handleChangeSort}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={handleChangeFilter}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,78 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const URL = "http://localhost:3000/stocks";
+
 class MainContainer extends Component {
+
+  state = {
+    sortBy: "Alphabetically",
+    filter: "Tech",
+    stocks: [],
+    portfolio: [],
+    portfolioId: 1,
+  }
+
+  componentDidMount() {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({stocks: data})
+    })
+  }
+
+  currentStocks = () => {
+    const filtered = this.filterStocks();
+    return (this.state.sortBy === "Price") ? this.sortStocks(filtered, "price") : this.sortStocks(filtered, "name")
+  }
+
+  filterStocks = () => {
+    return this.state.stocks.filter(stock => stock.type === this.state.filter)
+  }
+
+  sortStocks = (array, param) => {
+    return array.sort((a, b) => (a[param] > b[param]) ? 1 : -1)
+  }
+
+  chooseSort = sortValue => {
+    this.setState({sortBy: sortValue});
+  }
+
+  chooseFilter = filterValue => {
+    this.setState({filter: filterValue})
+  }
+
+  buyStock = stock => {
+    this.setState(prev => {
+      return ({
+        portfolio: [...prev.portfolio, {...stock, portfolioId: prev.portfolioId}],
+        portfolioId: prev.portfolioId + 1,
+      })
+    })
+  }
+
+  sellStock = stock => {
+    this.setState(prev => {
+      return ({
+        portfolio: prev.portfolio.filter(thisStock => thisStock !== stock)
+      })
+    })
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar sortBy={this.state.sortBy} onChooseSort={this.chooseSort} onChooseFilter={this.chooseFilter} />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer current={this.currentStocks()} onHandleBuy={this.buyStock} />
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer portfolio={this.state.portfolio} onHandleSell={this.sellStock} />
 
             </div>
           </div>
